48fe09843bca8e7b4503546c7bcf2bea82b27b1b
Phung Pham <phungpham@futurify.vn>
2025-05-02 13:10:38 +0700

    'update'

--- src/feature/LandingPage/index.js
+++ src/feature/LandingPage/index.js
@@ -1,8 +1,19 @@
 const LandingPage = () => {
-  return (<>
-    <img src={require(`assets/images/landing-page-web.png`)} alt="" />
-    </>)
-    
-}
+  return (
+    <>
+      <img
+        src={require('assets/images/landing-page-web.png')}
+        alt=""
+        className="hidden sm:block"
+      />
+
+      <img
+        src={require('assets/images/landing-page-mobile.png')}
+        alt=""
+        className="block sm:hidden"
+      />
+    </>
+  );
+};
 
 export default LandingPage;
